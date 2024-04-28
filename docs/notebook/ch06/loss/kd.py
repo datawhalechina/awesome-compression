840b66f14cabe095780604e94c00893545f1fb52
JinYu1998 <1870386153@qq.com>
2024-04-28 17:50:56 +0800

    update the kd code

---
+++ docs/notebook/ch06/loss/kd.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# kd_loss
+def loss(logits_student, logits_teacher, temperature):
+    log_pred_student = F.log_softmax(logits_student / temperature, dim=1)
+    pred_teacher = F.softmax(logits_teacher / temperature, dim=1)
+    loss_kd = F.kl_div(log_pred_student, pred_teacher, reduction="none").sum(1).mean()
+    loss_kd *= temperature**2
+    return loss_kd
